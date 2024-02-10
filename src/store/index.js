29e9886a8a0711f9c98e709e1097f85d61766092
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2024-02-10 18:41:16 +0530

    task 15

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import { createStoreHook } from "react-redux";
+const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter
     };
   }
 
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter
+    };
+  }
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter
     };
   }
 
